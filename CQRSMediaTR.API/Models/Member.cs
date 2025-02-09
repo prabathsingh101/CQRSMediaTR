cb658605cf366175ce35c627466233e7ca676944
DotNetCoreProjects <pappusingh954055@gmail.com>
2025-02-09 23:24:55 +0530

    done

--- CQRSMediaTR.API/Models/Member.cs
+++ CQRSMediaTR.API/Models/Member.cs
@@ -10,7 +10,6 @@ namespace CQRSMediaTR.API.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int MemberID { get; set; }
-
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
